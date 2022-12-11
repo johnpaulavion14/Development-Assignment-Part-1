bb02cd145557de82389a2c14a7ce138ee9093f59
johnpaulavion14 <johnpaul.bocatija.14@gmail.com>
2022-12-11 10:36:57 +0800

    Add methods in controller, routes, views for show answers

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,21 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def save_answer
+    Answer.create(answers_params)
+    redirect_to show_answers_path
+  end
+
+  def show_answer
+    @Answers = Answer.last
+  end
+
+
+  private
+
+  def answers_params
+    params.permit(:Q1, :Q2, :Q3)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
   root 'home#index'
+
+  post 'home/save_answer' 
+
+  get 'home/show_answer' , as:'show_answers'
 
 end
